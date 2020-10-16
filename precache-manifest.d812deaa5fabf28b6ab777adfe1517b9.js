2e85bffad5295a0511d6e9f01b6d539fe6977638
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-10-16 04:25:09 -0700

    Updates

---
+++ precache-manifest.d812deaa5fabf28b6ab777adfe1517b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe3dbad9eceef5e2263be4cf6e9a7a6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "08591a753f27cdd5daee",
+    "url": "/portfolio/static/css/main.a84c3724.chunk.css"
+  },
+  {
+    "revision": "c9e420e2e0d17a42d5ea",
+    "url": "/portfolio/static/js/2.21ee6658.chunk.js"
+  },
+  {
+    "revision": "ca30609d1e68bd85a38b5a3524022b3f",
+    "url": "/portfolio/static/js/2.21ee6658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08591a753f27cdd5daee",
+    "url": "/portfolio/static/js/main.ded77349.chunk.js"
+  },
+  {
+    "revision": "c53d9443373a56138b0b",
+    "url": "/portfolio/static/js/runtime-main.aad75174.js"
+  },
+  {
+    "revision": "68d0fb8ab135ce97d3a064fb40599dd0",
+    "url": "/portfolio/static/media/AbhijeetBhagat.68d0fb8a.pdf"
+  },
+  {
+    "revision": "a772d42b48b9f308b552c5b7059f7250",
+    "url": "/portfolio/static/media/CodeQuizDemo.a772d42b.gif"
+  },
+  {
+    "revision": "18eccc8418550914f1eb3df8653d5033",
+    "url": "/portfolio/static/media/RockPaperScissors.18eccc84.png"
+  },
+  {
+    "revision": "03fc25202f515fba0ab4c41d74d47a97",
+    "url": "/portfolio/static/media/TKRSEARCH.03fc2520.gif"
+  },
+  {
+    "revision": "b294dfb8661e226b97d4e87d1bfb478a",
+    "url": "/portfolio/static/media/Workday_Scheduler_demo-min.b294dfb8.gif"
+  },
+  {
+    "revision": "895bccc2321b75d26dfe2ddbfbbcf7a8",
+    "url": "/portfolio/static/media/abhi2.895bccc2.png"
+  },
+  {
+    "revision": "6d2a7c5d4c54323cd04da5b4256c3574",
+    "url": "/portfolio/static/media/bw_trans.6d2a7c5d.png"
+  },
+  {
+    "revision": "25900a88942adf4a98124c2e1a4ea47f",
+    "url": "/portfolio/static/media/meteopal.25900a88.gif"
+  }
+]);
